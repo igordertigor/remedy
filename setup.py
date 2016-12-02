3e663ddc74728b531ea9f50807d984fb0f172062
Ingo F <ingo.fruend@twentybn.com>
2016-12-02 15:11:01 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="remedy",
-      version="0.0.1",
+      version="0.0.2",
       author="Ingo Fruend",
       author_email="Ingo.Fruend@twentybn.com",
       description="Yet another markdown to reveal translator",
