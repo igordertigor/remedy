0de38a88bccc6bdb4d3c80b512c0bea02947a9f5
Ingo F <ingo.fruend@twentybn.com>
2016-12-02 14:36:14 +0100

    adding readme

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(name="remedy",
       version="0.0.1",
       author="Ingo Fruend",
       author_email="Ingo.Fruend@twentybn.com",
+      description="Yet another markdown to reveal translator",
+      long_description=open('README.md').read(),
       py_modules=['remedy'],
       install_requires=['jinja2==2.8', 'begins==0.9'],
       console_scripts=['remedy = remedy.py'])
