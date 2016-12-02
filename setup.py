71e99f44041c11c2a2ccb38fbb29f1380c432b58
Ingo F <ingo.fruend@twentybn.com>
2016-12-02 14:10:51 +0100

    adding main functionality

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name="remedy",
+      version="0.0.1",
+      author="Ingo Fruend",
+      author_email="Ingo.Fruend@twentybn.com",
+      py_modules=['remedy'],
+      console_scripts=['remedy = remedy.py'])
